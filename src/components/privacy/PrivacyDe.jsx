e29c580fd92540e75327432dfd9ebb2c3edd5951
Tobias Gassmann <tobias@gaszmann.de>
2024-09-06 15:17:31 +0200

    add missing files

---
+++ src/components/privacy/PrivacyDe.jsx
@@ -0,0 +1,13 @@
+const PrivacyDe = () => {
+    return (
+        <div>
+            <h1>Datenschutzerklärung</h1>
+            <p>
+                Dies ist die Datenschutzerklärung auf Deutsch. Bitte fügen Sie hier Ihre vollständigen Inhalte zur Datenschutzerklärung ein.
+            </p>
+            {/* More HTML content here */}
+        </div>
+    );
+};
+
+export default PrivacyDe;
---
+++ src/components/privacy/PrivacyEn.jsx
@@ -0,0 +1,13 @@
+const PrivacyEn = () => {
+    return (
+        <div>
+            <h1>Privacy Policy</h1>
+            <p>
+                This is the privacy policy in English. Please insert your full privacy policy content here.
+            </p>
+            {/* More HTML content here */}
+        </div>
+    );
+};
+
+export default PrivacyEn;
