9933a9326f6c76eb6ca69c524a9c2831e0041927
Tobias Gassmann <tobias@gaszmann.de>
2024-09-11 16:31:01 +0200

    working tailwind

--- packages/handlebars-site/tailwind.config.js
+++ packages/handlebars-site/tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  content: ['./templates/**/*.hbs'],
+  content: [
+    './src/**/*.{html,hbs,js}', // Make sure this points to your Handlebars templates and other files
+  ],
   theme: {
     extend: {},
   },
