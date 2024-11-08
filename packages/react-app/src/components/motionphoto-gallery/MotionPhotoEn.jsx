af8fca53be230d9d68e24815c2472d4c7987d3e8
Tobias Gassmann <tobias@gaszmann.de>
2024-11-08 13:27:42 +0100

    adjust wording

--- packages/react-app/src/components/motionphoto-gallery/MotionPhotoEn.jsx
+++ packages/react-app/src/components/motionphoto-gallery/MotionPhotoEn.jsx
@@ -10,7 +10,7 @@ export const MotionPhotoEn = () => {
 
             <p><strong><a href="https://motionphoto.gallery" target="_blank">motionphoto.gallery</a></strong> is a tool designed to extract video clips embedded in motion photos, a type of image file often captured on smartphones. These files contain both a still photo and a short video clip, recording the moments right before the photo was taken – useful if the shot was slightly delayed.</p>
 
-            <p>With <strong><a href="https://motionphoto.gallery" target="_blank">motionphoto.gallery</a></strong>, you can easily extract and save the video portion of your motion photos directly in your browser. The entire process happens locally on your computer, with no files uploaded.</p>
+            <p>With <strong><a href="https://motionphoto.gallery" target="_blank">motionphoto.gallery</a></strong>, you can easily extract and save the video portion of your motion photos directly in your browser. The entire process happens locally on your computer.</p>
 
             <p>Since the extraction is handled within your browser, the process may use more of your computer's processing power, especially for larger or high-resolution motion photos, and could cause it to run hotter than usual.</p>
 
